71ab7d7fc7b375cb8f6f19d9ae8b3a4d972438bd
Will Usher <w.usher@ucl.ac.uk>
2014-03-07 11:33:13 +0000

    PEP8: Changed spacing from tabs to spaces.

--- boids.py
+++ boids.py
@@ -16,33 +16,33 @@ boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	number_of_boids = len(xs)
-	array_of_boids =  range(number_of_boids)
+    xs,ys,xvs,yvs=boids
+    number_of_boids = len(xs)
+    array_of_boids =  range(number_of_boids)
 	
-	# Fly towards the middle
-	for i in array_of_boids:
-		for j in array_of_boids:
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/number_of_boids
-	for i in array_of_boids:
-		for j in array_of_boids:
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/number_of_boids
-	# Fly away from nearby boids
-	for i in array_of_boids:
-		for j in array_of_boids:
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in array_of_boids:
-		for j in array_of_boids:
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/number_of_boids
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/number_of_boids
-	# Move according to velocities
-	for i in array_of_boids:
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
+    # Fly towards the middle
+    for i in array_of_boids:
+        for j in array_of_boids:
+            xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/number_of_boids
+    for i in array_of_boids:
+        for j in array_of_boids:
+            yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/number_of_boids
+    # Fly away from nearby boids
+    for i in array_of_boids:
+        for j in array_of_boids:
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+                xvs[i]=xvs[i]+(xs[i]-xs[j])
+                yvs[i]=yvs[i]+(ys[i]-ys[j])
+    # Try to match speed with nearby boids
+    for i in array_of_boids:
+        for j in array_of_boids:
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
+                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/number_of_boids
+                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/number_of_boids
+    # Move according to velocities
+    for i in array_of_boids:
+        xs[i]=xs[i]+xvs[i]
+        ys[i]=ys[i]+yvs[i]
 
 
 figure=plt.figure()
@@ -50,12 +50,11 @@ axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
 scatter=axes.scatter(boids[0],boids[1])
 
 def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
+    update_boids(boids)
+    scatter.set_offsets(zip(boids[0],boids[1]))
 
 
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
 if __name__ == "__main__":
     plt.show()
