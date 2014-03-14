e74d174a73ed809295fccc69e46a0714ade30dd3
Will Usher <w.usher@ucl.ac.uk>
2014-03-14 17:40:51 +0000

    Finished Boid and Boids class refactorisation

--- boids.py
+++ boids.py
@@ -3,20 +3,31 @@ A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid
 for use as an exercise on refactoring.
 """
 
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import random
+
+
 class Boid(object):
     """
-    A class of object to represent the birds
+    A class of object to represent a bird
     Parameters:
         number_of_boids -   class member variable which counts the
                             number of boids initialised
-
-
+        x               -   x position
+        y               -   y position
+        xv              -   x vector
+        yv              -   y vector
     """
 
     number_of_boids = 0
 
-    def __init__(self):
+    def __init__(self, x, y, xv, yv):
         Boid.number_of_boids += 1
+        self.x = x
+        self.y = y
+        self.xv = xv
+        self.yv = yv
 
     @classmethod
     def get_number_of_boids(self):
@@ -32,19 +43,56 @@ class Boids(object):
         yvs -   Vector of y vectors
     """
 
-    def __init__(self,xs,ys,xvs,yvs):
-        self.xs = xs
-        self.ys = ys
-        self.xvs = xvs
-        self.yvs = yvs
+    def __init__(self,number_of_boids,repulsion = 100, \
+                    attraction = 0.01, speed_threshold = 10000, \
+                    acceleration = 0.125):
+        self.boids = []
+        for i in range(1,number_of_boids):
+            self.boids.append(Boid(
+                        random.uniform(-450,50.0), \
+                        random.uniform(300.0,600.0), \
+                        random.uniform(0,10.0), \
+                        random.uniform(-20.0,20.0)
+                        )
+                        )
+        self.repulsion = repulsion
+        self.attraction = attraction
+        self.speed_threshold = speed_threshold
+        self.acceleration = acceleration
+        self.number_of_boids = Boid.get_number_of_boids()
 
+    def move_boids_middle(self):
+        for i in self.boids:
+            for j in self.boids:
+                i.xv = i.xv + \
+                    (j.x - i.x) * self.attraction / self.number_of_boids
+                i.yv = i.yv + \
+                    (j.y - i.y) * self.attraction / self.number_of_boids
 
+    def boids_avoid_others(self):
+        for i in self.boids:
+            for j in self.boids:
+                if (j.x - i.x)**2 + (j.y - i.y)**2 < self.repulsion:
+                    i.xv = i.xv + (i.x - j.x)
+                    i.yv = i.yv + (i.y - j.y)
 
+    def match_speed(self):
+        for i in self.boids:
+            for j in self.boids:
+                if (j.x - i.x)**2 + (j.y - i.y)**2 < self.speed_threshold:
+                    i.xv = i.xv + (j.xv - i.xv) * self.acceleration \
+                                / self.number_of_boids
+                    i.yv = i.yv + (j.yv - i.yv) * self.acceleration \
+                                / self.number_of_boids
 
-
-from matplotlib import pyplot as plt
-from matplotlib import animation
-import random
+    def update_boids(self):
+        self.move_boids_middle()
+        self.boids_avoid_others()
+        self.match_speed()
+        # Move according to velocities
+        for i in self.boids:
+            i.x = i.x + i.xv
+            i.y = i.y + i.yv
 
 # Deliberately terrible code for teaching purposes
 
